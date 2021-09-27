5118a53138c84f49a24e0beb17c84fb20de7e579
OroborosD <diego-siebra@hotmail.com>
2021-09-27 15:33:14 -0300

    css do campo comentario e correções no form

--- post/forms.py
+++ post/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ComentarioForm(forms.Form):
-    autor = forms.CharField()
-    comentario = forms.CharField()
+    autor = forms.CharField(label='Nome' ,min_length=4, max_length=50)
+    comentario = forms.CharField(label="comentario", min_length=1, widget=forms.Textarea)
--- post/views.py
+++ post/views.py
@@ -6,7 +6,7 @@ from .forms import ComentarioForm
 # Create your views here.
 
 def post(request, slug):
-    post_descricao = Post.objects.get(slug=slug)
+    post_descricao = Post.objects.get(slug=slug)#pega todos os ativutos do post com o slug igual
     id = int(post_descricao.pk)
 
     if request.method == 'POST':
@@ -15,18 +15,20 @@ def post(request, slug):
         #                          comentario=form_comentario['comentario'],
         #                           post_id = Post.objects.get(slug=slug))#o objeto de onde vem a Oé Post
         
+        # se recebeu um post, peega os dados do post e coloca na varial form_post
         form_post = ComentarioForm(request.POST)
 
-        if form_post.is_valid():
+        if form_post.is_valid():#verifica se os campos do formulçario são validos
             print(form_post.cleaned_data['autor'])
-            Comentario.objects.create(autor= form_post.cleaned_data['autor'], 
+            Comentario.objects.create(autor= form_post.cleaned_data['autor'],  #salva o post no database.
                                   comentario=form_post.cleaned_data['comentario'],
                                    post_id = Post.objects.get(slug=slug))#o objeto de onde vem a Oé Post
             
         
-        return HttpResponseRedirect(f'/lista/{slug}')
+            return HttpResponseRedirect(f'/lista/{slug}')#redireciana para a pagina, ccaso de certo.
     
-    form = ComentarioForm()
+    else:
+        form_post = ComentarioForm()
 
     comment = Comentario.objects.filter(post_id= id)# para mostrar todos os compentarios do post.
     tags = post_descricao.tags.all()
@@ -34,7 +36,7 @@ def post(request, slug):
         'post':post_descricao,
         'post_tags':tags,
         'comments':comment,
-        'form': form
+        'forms': form_post
     }
     return render(request, 'post/post.html',context)
 
